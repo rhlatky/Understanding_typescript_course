68a3fecbebe922b4d7ecc584c49021a9b6f1ed0a
Radoslav.Hlatky <radoslav.hlatky@oneguard.email>
2021-06-27 21:39:34 +0200

    section 3

--- section-3/app.ts
+++ section-3/app.ts
@@ -1,3 +1,14 @@
-const userName = 'Max';
-
-console.log(userName);
+const button = document.querySelector('button');
+function add (n1: number, n2: number) {
+    if(n1 + n2 > 0) {
+        return n1 + n2;
+    }
+   // return
+}
+function clickHandler(message: string) {
+    // let username = 'max';
+    console.log('Clicked! ' + message);
+}
+if(button) {
+    button.addEventListener('click', clickHandler.bind(null, 'Youre welcome'))
+}
