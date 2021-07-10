68cc9cfabe8513b3c8f544f17a86197f64785dc9
Radoslav Hlatký <radoslav.hlatky@oneguard.email>
2021-07-10 22:09:24 +0200

    section 7 - end

--- section-7/src/app.ts
+++ section-7/src/app.ts
@@ -34,4 +34,63 @@ function countAndDescribe<T extends Lengthy>(element: T): [T, string] {
 	return [element, descriptionText]
 }
 
-console.log(countAndDescribe('Hi there!'))
+console.log(countAndDescribe(['sports', 'cooking']));
+
+function extractAndConvert<T extends object, U extends keyof T>(obj: T, key: U) {
+	return 'Value: ' + obj[key];
+}
+
+extractAndConvert({name: 'Max'}, 'name');
+
+class DataStorage<T extends string | number | boolean> {
+	private data: T[] = [];
+
+	addItem(item: T) {
+		this.data.push(item);
+	}
+
+	removeItem(item: T) {
+		this.data.splice(this.data.indexOf(item), 1);
+	}
+
+	getItems() {
+		return [...this.data];
+	}
+}
+
+const textStorage = new DataStorage<string>();
+textStorage.addItem('Max');
+textStorage.addItem('Manu');
+textStorage.removeItem('Max');
+console.log(textStorage.getItems());
+
+const numberStorage = new DataStorage<number>();
+numberStorage.addItem(1);
+numberStorage.addItem(2);
+numberStorage.removeItem(2);
+console.log(numberStorage.getItems())
+
+// const objStorage = new DataStorage<object>();
+// objStorage.addItem({name: 'Max'});
+// objStorage.addItem({name: 'Manu'});
+// objStorage.removeItem({name: 'Max'});
+// console.log(objStorage.getItems());
+
+interface CourseGoal {
+	title: string;
+	description: string;
+	completeUntil: Date;
+}
+
+function createCourseGoal(title: string, description: string, date: Date): CourseGoal {
+	let courseGoal: Partial<CourseGoal> = {};
+	courseGoal.title = title;
+	courseGoal.description = description;
+	courseGoal.completeUntil = date;
+
+	return courseGoal as CourseGoal ;
+}
+
+const names: Readonly<string[]> = ['Max', 'Anna'];
+// names.push('Manu');
+// names.pop();
