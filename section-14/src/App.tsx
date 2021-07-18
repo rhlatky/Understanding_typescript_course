e68063a6730aa579d28469331dcd85eb0e152c81
Radoslav Hlatky <radoslav.hlatky@oneguard.email>
2021-07-18 20:02:17 +0200

    section-14 end

---
+++ section-14/src/App.tsx
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+
+import TodoList from './components/TodoList';
+import NewTodo from './components/NewTodo';
+import {Todo} from "./todo.model";
+
+const App: React.FC = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const todoAddHandler = (text: string) => {
+    setTodos( prevTodos => [
+        ...prevTodos,
+        {
+            id: Math.random().toString(),
+            text: text
+        }
+        ]
+    );
+  };
+  const todoDeleteAddHandler = (todoId: string) => {
+    setTodos( prevTodos => prevTodos.filter(todo => todo.id !== todoId));
+  };
+
+
+
+  return (
+    <div className="App">
+        <NewTodo onAddTodo={todoAddHandler} />
+        <TodoList items={todos} onDeleteTodo={todoDeleteAddHandler} />
+    </div>
+  );
+};
+
+export default App;
